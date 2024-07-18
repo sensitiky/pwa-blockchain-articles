9d60b69c56c4dc1f042ce48cb982e053ff55d082
Sensitiky <mariomcorrea3@gmail.com>
2024-07-18 09:44:50 -0300

    Entity

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { UsersController } from './auth/users/users.controller';
+import { PostsController } from './auth/posts/posts.controller';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [DatabaseModule, AuthModule],
-  controllers: [UsersController],
+  controllers: [UsersController,PostsController],
 })
 export class AppModule {}
