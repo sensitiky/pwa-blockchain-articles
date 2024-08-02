4386fe896087b4d090e9ec58c1c05271f8f6c8bd
Sensitiky <mariomcorrea3@gmail.com>
2024-08-02 14:43:32 -0300

    Server

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://blogchain-eight.vercel.app','https://localhost:3001'],
+    origin: ['http://localhost:3000', 'https://blogchain-eight.vercel.app','https://localhost:3001','https://www.blogchain.tech/'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
