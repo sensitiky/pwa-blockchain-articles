d84e8db151be0bc99ebdae41a99a48a449fe6fc4
Sensitiky <mariomcorrea3@gmail.com>
2024-08-14 00:55:36 -0300

    Update session

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -20,7 +20,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 	inject:[ConfigService],
 	useFactory: async (configService: ConfigService) =>({
 	secret: configService.get<string>('JWT_SECRET'),
-	signOptions: { expiresIn: '60m' },
+	signOptions: { expiresIn: '360m' },
     }),
    }),
   ],
