35be97b1fd54fd49ae66a441a83e2175b43fc7fd
sensitiky <mariomcorrea3@gmail.com>
2024-09-25 22:29:21 -0300

    final updates

--- backend/src/modules/database.module.ts
+++ backend/src/modules/database.module.ts
@@ -19,20 +19,33 @@ import { FavoriteSubscriber } from '../services/listenerFavorites.service';
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_DATABASE'),
-        entities: [User, Post, Comment, Category, Tag, Favorite],
-        synchronize: true,
-        logging: true,
-        //Fix for SSL connection must be removed in development
+      useFactory: async (configService: ConfigService) => {
+        const dbHost = configService.get<string>('DB_HOST');
+        const dbPort = configService.get<number>('DB_PORT');
+        const dbUsername = configService.get<string>('DB_USERNAME');
+        const dbPassword = configService.get<string>('DB_PASSWORD');
+        const dbName = configService.get<string>('DB_DATABASE');
 
-        subscribers: [CommentSubscriber, FavoriteSubscriber],
-      }),
+        console.log('DB_HOST:', dbHost);
+        console.log('DB_PORT:', dbPort);
+        console.log('DB_USERNAME:', dbUsername);
+        console.log('DB_PASSWORD:', dbPassword);
+        console.log('DB_DATABASE:', dbName);
+
+        return {
+          type: 'postgres',
+          host: dbHost,
+          port: dbPort,
+          username: dbUsername,
+          password: dbPassword,
+          database: dbName,
+          entities: [User, Post, Comment, Category, Tag, Favorite],
+          synchronize: true,
+          logging: true,
+          // Fix for SSL connection must be removed in development
+          subscribers: [CommentSubscriber, FavoriteSubscriber],
+        };
+      },
       inject: [ConfigService],
     }),
     TypeOrmModule.forFeature([User, Post, Comment, Category, Tag, Favorite]),
